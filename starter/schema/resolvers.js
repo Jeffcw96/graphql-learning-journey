8d601b66d2294689b94bdd062006eb6385ed6045
Jeffcw96 <changcheewah96@gmail.com>
2023-05-15 20:40:12 +0800

    Add instructions and readme

--- starter/schema/resolvers.js
+++ starter/schema/resolvers.js
@@ -1,11 +1,14 @@
-const { Users } = require("../data");
+const { Users, Sports } = require("../data");
 
 const resolvers = {
   Query: {
     users: () => {
       return Users;
     },
+    // Add user query by id resolver
   },
+  // Add favouriteSport custom query resolver for User type
+  // Add mutation to create and update user
 };
 
 module.exports = { resolvers };
