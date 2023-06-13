d2febe790629a019be6841fe2486e4c3e4837aec
Jeffcw96 <changcheewah96@gmail.com>
2023-06-13 19:56:52 +0800

    fix bug

--- 02-react-graphql/starter/src/main.jsx
+++ 02-react-graphql/starter/src/main.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000/",
-  cache: new InMemoryCache(),
-});
+// Configure Apollo Client below
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>
   </React.StrictMode>
 );
